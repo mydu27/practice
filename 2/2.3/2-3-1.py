b1670a2b9ffb09d33a64f805bc01f9e4b68e53db
dudu0327 <chenjiageng914@geekpark.net>
2018-07-23 19:05:47 +0800

    修改错误代码

--- 2/2.3/2-3-1.py
+++ 2/2.3/2-3-1.py
@@ -1,7 +1,7 @@
 # 求解二元一次方程的另一个解
-a = 5
-b = 4
-c = 3
+a = 2
+b = -5
+c = 2
 determ = (b * b - 4 * a * c) ** 0.5
-x = (-b - determ) / 2 * a
+x = (-b - determ) / (2 * a)
 print(x)
--- 3/3.1/3-1-2.py
+++ 3/3.1/3-1-2.py
@@ -1,9 +1,9 @@
 def quad(a, b, c):
     determ = (b * b - 4 * a * c) ** 0.5
-    x1 = (-b + determ) / 2 * a
-    x2 = (-b - determ) / 2 * a
+    x1 = (-b + determ) / (2 * a)
+    x2 = (-b - determ) / (2 * a)
     print(f'The x1 value is {x1}')
     print(f'The x2 value is {x2}')
 
 
-quad(1, -3, 2)
+quad(3, -2, -1)
