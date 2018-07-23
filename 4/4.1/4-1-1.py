a6fc564227f83ef55f18923bead0a200d0295dff
dudu0327 <chenjiageng914@geekpark.net>
2018-07-23 20:09:45 +0800

    增加4.1习题

---
+++ 4/4.1/4-1-1.py
@@ -0,0 +1,9 @@
+def main():
+    your_number = int(input('think a number and input it: '))
+    if your_number >= 1 and your_number <= 100:
+        print('你输入的数字在1-100之间')
+    else:
+        print('你输入的数字不在1-100之间')
+
+
+main()
---
+++ 4/4.1/4-1-3.py
@@ -0,0 +1,9 @@
+def main():
+    n = int(input('请输入一个整数：'))
+    if n % 7 == 0:
+        print('你输入的数字可以被7整除')
+    else:
+        print('你输入的数字不能被7整除')
+
+
+main()
