ce8facc93edfda6d8cdba2b93885760d00396b60
dudu0327 <chenjiageng914@geekpark.net>
2018-07-23 19:39:07 +0800

    修改3-2-2

--- 3/3.2/3-2-2.py
+++ 3/3.2/3-2-2.py
@@ -1,11 +1,20 @@
 # 修改后，用户必须输入整数值
+def answer(x):
+    if not x.isdigit():
+            print('your input has mistake')
+            main()
+
+
 def main():
-    a = int(input('Enter the value of the x-square coefficient: '))
-    b = int(input('Enter the value of the x coefficient: '))
-    c = int(input('Enter the value of the constant: '))
-    determ = (b * b - 4 * a * c) ** 0.5
-    x1 = (-b + determ) / (2 * a)
-    x2 = (-b - determ) / (2 * a)
+    a = input('Enter the value of the x-square coefficient: ')
+    answer(a)
+    b = input('Enter the value of the x coefficient: ')
+    answer(b)
+    c = input('Enter the value of the constant: ')
+    answer(c)
+    determ = (int(b) * int(b) - 4 * int(a) * int(c)) ** 0.5
+    x1 = (-int(b) + determ) / (2 * int(a))
+    x2 = (-int(b) - determ) / (2 * int(a))
     print('Answer are', x1, 'and', x2)
 
 
