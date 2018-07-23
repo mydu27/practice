ac17f2a3a74e17b80b4dadbcd5aecbee4a1df7d9
dudu0327 <chenjiageng914@geekpark.net>
2018-07-23 16:13:16 +0800

    practice 2.3

---
+++ 2/2.3/2-3-1.py
@@ -0,0 +1,6 @@
+a = 5
+b = 4
+c = 3
+determ = (b * b - 4 * a * c) ** 0.5
+x = (-b - determ) / 2 * a
+print(x)
---
+++ 2/2.3/2-3-2.py
@@ -0,0 +1,5 @@
+# 变量love没有定义
+he_loves = 10
+she_loves = -10
+Love = 2
+they_love = he_loves * she_loves +love
